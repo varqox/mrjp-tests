9160a316155199f1771ac0e5bd387d800f7d6588
Krzysztof Małysa <varqox@gmail.com>
2021-01-06 14:41:33 +0100

    Enhanced tester: it now accepts individual .lat files and produces extensive report for them

--- test.sh
+++ test.sh
@@ -15,20 +15,23 @@ tmp_test_prog="${tmp_dir}/test"
 compiler_stderr="${tmp_dir}/compiler_stderr"
 prog_output="${tmp_dir}/prog_output"
 valgrind_log_file="${tmp_dir}/valgrind_log"
+diff_output="${tmp_dir}/diff_output"
 
 fail() {
     # $1 -- reason
     echo -e "\033[1;31mFAILED\033[m ($1)"
-    echo -e "  \033[2m${latc} ${test_lat}\033[m"
+    echo -e "  \033[2m$0 ${latc} ${test_lat}\033[m"
 }
 
 good_test_compilation_failed() {
     fail "compiler exited with code ${compiler_ec}, but 0 was expected"
+    if $single_test; then cat "${compiler_stderr}"; fi
     return 1
 }
 good_test_compilation_succeeded() {
     if [ "$(head -n 1 ${compiler_stderr})" != "OK" ]; then
         fail "first line of compiler's stderr is not \"OK\""
+        if $single_test; then cat "${compiler_stderr}"; fi
         return 1
     fi
     if ${use_valgrind}; then
@@ -48,16 +51,19 @@ good_test_compilation_succeeded() {
     fi
     if [ "${ec}" != "${expected_ec}" ]; then
         fail "program exited with code ${ec}, but the expected code is ${expected_ec}"
+        if $single_test; then cat "${prog_output}"; fi
         return 1
     fi
     if test -f "${test_output}"; then
-        if ! diff "${test_output}" "${prog_output}" > /dev/null; then
+        if ! diff "${test_output}" "${prog_output}" > "${diff_output}"; then
             fail "program output differs from the expected output"
+            if $single_test; then cat "${diff_output}"; fi
             return 1
         fi
     fi
     if ! tail -n 1 "${valgrind_log_file}" | grep '== ERROR SUMMARY: 0 errors' -q; then
         fail "valgrind found memory errors"
+        if $single_test; then cat "${valgrind_log_file}"; fi
         return 1
     fi
     return 0
@@ -65,29 +71,35 @@ good_test_compilation_succeeded() {
 bad_test_compilation_failed() {
     if [ "$(head -n 1 ${compiler_stderr})" != "ERROR" ]; then
         fail "first line of compiler's stderr is not \"ERROR\""
+        if $single_test; then cat "${compiler_stderr}"; fi
         return 1
     fi
     if ! grep -qP "[Ee]rror" "${compiler_stderr}"; then
         fail "compiler's stderr does not contain error description"
+        if $single_test; then cat "${compiler_stderr}"; fi
         return 1
     fi
     return 0
 }
 bad_test_compilation_succeeded() {
     fail "compiler exited with code 0, but an error was expected"
+    if $single_test; then cat "${compiler_stderr}"; fi
     return 1
 }
 warning_test_compilation_failed() {
+    if $single_test; then cat "${compiler_stderr}"; fi
     fail "compiler exited with code ${compiler_ec}, but 0 was expected"
     return 1
 }
 warning_test_compilation_succeeded() {
     if [ "$(head -n 1 ${compiler_stderr})" != "OK" ]; then
         fail "first line of compiler's stderr is not \"OK\""
+        if $single_test; then cat "${compiler_stderr}"; fi
         return 1
     fi
     if ! grep -qP "[Ww]arning" "${compiler_stderr}"; then
         fail "compiler's stderr does not contain warning description"
+        if $single_test; then cat "${compiler_stderr}"; fi
         return 1
     fi
     return 0
@@ -98,28 +110,34 @@ failed_count=0
 
 test_on_dir() {
     # $1 -- dir
-    abs_dir=$(cd -P -- "$1" && pwd -P)
-    for test in $(find "${1}/" -type f -printf '%P\n' | sort | grep -P '\.lat$'); do
-        test=${test%.lat}
-        test_lat="${1}/${test}.lat"
-        test_input="${1}/${test}.input"
-        test_output="${1}/${test}.output"
-        test_exit_code="${1}/${test}.exit_code"
+    case "${1}" in
+        *.lat) single_test=true ;;
+        *)  single_test=false ;;
+    esac
+    for test_lat in $(find "${1}" -type f | sort | grep -P '\.lat$'); do
+        test_name=${test_lat%.lat}
+        case "${1}" in
+            */) test_name=${test_name#"${1}"} ;;
+            *) test_name=${test_name#"${1}/"} ;;
+        esac
+        test_input="${test_lat%.lat}.input"
+        test_output="${test_lat%.lat}.output"
+        test_exit_code="${test_lat%.lat}.exit_code"
 
-        printf "%-50s " "${test}"
+        printf "%-60s " "${test_name}"
 
         if test -f "${test_output}"; then
             comp_fail=good_test_compilation_failed
             comp_succ=good_test_compilation_succeeded
-            echo -n 'as good: '
-        elif echo "${abs_dir}/${test}" | grep -qP '/warnings?/'; then
+            echo -n 'kind:good: '
+        elif readlink -f "${test_lat}" | grep -qP '/warnings?/'; then
             comp_fail=warning_test_compilation_failed
             comp_succ=warning_test_compilation_succeeded
-            echo -n 'as warn: '
+            echo -n 'kind:warning: '
         else
             comp_fail=bad_test_compilation_failed
             comp_succ=bad_test_compilation_succeeded
-            echo -n 'as bad:  '
+            echo -n 'kind:bad: '
         fi
 
         cp "${test_lat}" "${tmp_test_lat}"
